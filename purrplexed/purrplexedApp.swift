5b137a70130bc616a7d6cdbdd8d5f6e46ca667d4
Frank Walsh <fkw155@mun.ca>
2025-09-20 22:20:56 -0700

    Initial Commit

---
+++ purrplexed/purrplexedApp.swift
@@ -0,0 +1,17 @@
+//
+//  purrplexedApp.swift
+//  purrplexed
+//
+//  Created by Francis Walsh on 9/20/25.
+//
+
+import SwiftUI
+
+@main
+struct purrplexedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
